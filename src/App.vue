6e3614a0e4367d334372b4257dc4c8b0bae4feec
Clara Luz <102390458+claraluzlopes@users.noreply.github.com>
2025-05-20 14:49:11 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+const nessage = /// <reference path="" />
+
+
+</script>
+
+<template>
+  <main>
+    <h1></h1>
+  </main>
+</template>
+
+<style scoped>
+main{
+  max-width: 800px;
+  margin: 1rem auto;
+}
+</style>
